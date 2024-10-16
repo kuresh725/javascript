283227696b3c2edb1616b30db3e12d841afd56fb
kuresh725 <abdirahmankuresh50@gmail.com>
2024-10-16 15:57:54 +0300

    Commit1

---
+++ myscript.js
@@ -0,0 +1,9 @@
+
+<p id="demo">A paragraph.</p>
+<button type="button" onclick="myFunction()">Try it</button>
+<p>This example links to "myScript.js".</p>
+<p>(myFunction is stored in "myScript.js")</p>
+
+
+</body>
+</html>
